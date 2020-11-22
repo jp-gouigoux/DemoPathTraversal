f5a46bc7b6ada20174e238e84340af6daffa9b1c
JP Gouigoux <jp.gouigoux@free.fr>
2020-11-23 00:05:54 +0100

    Ajoutez des fichiers projet.

---
+++ PathTraversalWebDemo/Controllers/WelcomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathTraversalWebDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Get()
+        {
+            // Suppose this data comes from authentication for customer SALVIA
+            string CustomerCode = "00000032";
+            string CustomerName = "SALVIA";
+            string CustomerAgent = "JP Gouigoux";
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<h1>Bienvenue, " + CustomerName + " !</h1>");
+            sb.Append("<h3>Votre correspondant</h3>");
+            sb.Append("<p>" + CustomerAgent + "</p>");
+            sb.Append("<h3>Vos documents</h3>");
+            sb.Append("<iframe src='" + Request.Scheme + "://" + Request.Host + "/" + CustomerCode + "/' width='100%' height='100%' frameborder='0'/>");
+
+            return new ContentResult {
+                ContentType = "text/html",
+                Content = sb.ToString(),
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+    }
+}
---
+++ PathTraversalWebDemo/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PathTraversalWebDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDirectoryBrowser();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseDirectoryBrowser();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
