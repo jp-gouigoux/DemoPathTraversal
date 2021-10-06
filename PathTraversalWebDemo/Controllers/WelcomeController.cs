be0dfd4fc2080d6138c5c045e26515247eaa0708
JP Gouigoux <jp.gouigoux@salviadeveloppement.com>
2021-10-06 17:34:50 +0200

    Correction des failles ou indications

--- PathTraversalWebDemo/Controllers/WelcomeController.cs
+++ PathTraversalWebDemo/Controllers/WelcomeController.cs
@@ -18,10 +18,19 @@ namespace PathTraversalWebDemo.Controllers
         [HttpGet("content")]
         public string GetContent(string fichier)
         {
+            // Defence In Depth : plusieurs couches de sécurité plutôt qu'une seule
+            if (fichier.Contains("..")) throw new ArgumentException();
+
             // Ce code-ci est suffisamment brutal pour permettre l'accès par http://localhost:56253/welcome/content?fichier=/Windows/System32/drivers/etc/hosts
             // Mais ce n'est pas nécessairement le cas si on déploie l'application sur un "vrai" serveur web, car seul IIS Express fonctionne sous l'utilisateur courant
             if (string.IsNullOrEmpty(fichier)) return string.Empty;
             string nomFichier = Path.Combine(@"C:\Code\Github\DemoPathTraversal\PathTraversalWebDemo\wwwroot", "00000032", fichier);
+
+            // Ajout d'une protection aidée par la canonicalisation du nom de fichier (en vrai, pas de "magical chain" bien sûr...)
+            string nomFichierReduit = Path.GetFullPath(nomFichier);
+            if (!nomFichierReduit.ToLower().StartsWith(@"c:\code\github\demopathtraversal\pathtraversalwebdemo\wwwroot\"))
+                throw new ArgumentException();
+
             return System.IO.File.ReadAllText(nomFichier);
         }
 
@@ -40,7 +49,13 @@ namespace PathTraversalWebDemo.Controllers
             sb.Append("<h3>Vos documents</h3>");
             sb.Append("<iframe src='" + Request.Scheme + "://" + Request.Host + "/" + CustomerCode + "/' width='100%' height='100%' frameborder='0'/>");
 
-            return new ContentResult {
+            // Pour éviter la faille ci-dessus, la canonicalisation de fichier n'est pas de mise et il faut d'autres méthodes :
+            // - Authentification + impersonification + droits sur les fichiers
+            // - Suppression du service de fichiers statiques et de discovery par le serveur web (seulement pour ressources publiques) et
+            //   remplacement par du code sur mesure qui récupère les fichiers dans un dossier accessible par le serveur IIS
+
+            return new ContentResult
+            {
                 ContentType = "text/html",
                 Content = sb.ToString(),
                 StatusCode = (int)HttpStatusCode.OK
